7b3fa27d8b2a9ebec87f68a56f8c7c0fc755e7b9
Your Name <jpark1917@gmail.comecho>
2021-09-16 14:12:59 -0700

    pre processing

--- Cases_did.R
+++ Cases_did.R
@@ -2,4 +2,74 @@
 # CRM-15002 
 # September 14, 2021
 # cases did
+# https://jira.arizona.edu/browse/CRM-15002
 
+library(dplyr)
+library(ggplot2)
+library(reshape)
+library(reshape2)
+library(RColorBrewer)
+library(tibble)
+library(tidyverse)
+library(ggstatsplot)
+library(lubridate)
+library(forcats)
+library(purrr)
+# read in data
+setwd("~/Documents/Trellis/CRM-15002/data")
+
+# cases_did <- read.csv("cases_did.csv", header = TRUE, na.strings=c("","NA"))
+
+# table(cases_did$RecordTypeId)
+# # try to filter the data
+# # cases_type <- cases_did %>%
+# #   dplyr::filter(cases_did, RecordTypeId == "0122S0000002Tl3QAE")
+# cases_type <- mutate(cases_did, RecordType = (RecordTypeId == '0122S0000002Tl3QAE'))
+# 
+# cases_type2 <- subset(cases_type, RecordType == TRUE)
+# write.csv(cases_type2, file = "cases_type2.csv")
+
+cases_type2 <- read.csv("cases_type2.csv", header = TRUE, na.strings=c("","NA"))
+
+# changing date and time from UTC to AZ time
+# date_value4 = cases_type2$CreatedDate
+# 
+# Date_Time4 = ymd_hms(cases_type2$CreatedDate)
+# # head(Date_Time) #2020-07-02 21:56:42 UTC
+# cases_type2 <- dplyr::mutate(cases_type2, Date_Time4)
+# 
+# # change to AZ time 
+# t4 <- as.POSIXct(Date_Time4, tz = "GMT")
+# attributes(t4)$tzone
+# AZ_time4 <- lubridate::with_tz(t4, "MST")
+# 
+# cases_type2 <- dplyr::mutate(cases_type2, AZ_time4)
+# 
+# # convert time to decimals 
+# tm4.dechr <- hour(cases_type2$AZ_time4) + minute(cases_type2$AZ_time4)/60 + 
+#   second(cases_type2$AZ_time4)/3600
+cases_type2 <- cases_type2 %>% 
+   mutate(AZ_date4 = as.Date(AZ_time4))
+# 
+# cases_type2 <- subset(cases_type2, select = -c(1, 2))
+# write.csv(cases_type2, file = "cases_type2.csv")
+
+# summary statistics
+summary(cases_type2)
+table(cases_type2$Origin) # 8 categories
+
+# create histogram
+
+p3 <- cases_type2 %>% 
+  drop_na(Origin) %>% 
+  ggplot(aes(x=AZ_date4, fill = Origin)) +
+  geom_histogram(binwidth = 5) + 
+  scale_fill_brewer(palette = "Set2") +
+  labs(x = "Date", y="Count")
+
+p3 <- p3 + scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 week",
+                        date_labels = "%b-%y") + labs(title = "Histogram of Cases over Time", 
+                                                      subtitle = "full data", fill = "Origin")
+p3
+
+### 
---
+++ chattran.R
@@ -0,0 +1,75 @@
+# enrollment data
+# september 15, 2021
+library(dplyr)
+library(ggplot2)
+library(reshape)
+library(reshape2)
+library(RColorBrewer)
+library(tibble)
+library(tidyverse)
+library(ggstatsplot)
+library(lubridate)
+library(forcats)
+library(purrr)
+
+setwd("~/Documents/Trellis/CRM-15002/data")
+
+chattran <- read.csv("chattran.csv", header = TRUE, na.strings=c("","NA"))
+
+# recode developer name
+# chattran$DeveloperName = recode_factor(chattran$LiveChatDeployment.DeveloperName, 
+                                       # "X24_7_Chat" = "24/7",
+                                       # "University_Services_Chat" = "Other",
+                                       # "Think_Tank_Chat" = "Other",
+                                       # "Study_Abroad_Chat" = "Other",
+                                       # "SOS_Chat" = "SOS",
+                                       # "SECD_Chat" = "Other",
+                                       # "Registrar_Chat" = "Registrar",
+                                       # "Psych_Dept_Chat" = "Other",
+                                       # "OSFA_Chat" = "OSFA", 
+                                       # "LifeLab_Chat" = "Other",
+                                       # "College_of_Engineering_Chat" = "Other")
+### time stamps
+# date_value3 = chattran$CreatedDate
+# day_of_the_week3 = date(date_value3) %>%
+#   wday()
+# # day_of_the_week
+# 
+# chattran = dplyr::mutate(chattran, day_of_the_week3)
+# chattran$day_of_the_week <- recode_factor(chattran$day_of_the_week, 
+#                                           "1" = "Sunday", 
+#                                           "2" = "Monday",
+#                                           "3" = "Tuesday",
+#                                           "4" = "Wednesday",
+#                                           "5" = "Thursday",
+#                                           "6" = "Friday",
+#                                           "7" = "Saturday")
+
+# check that the date time is in the right time zone
+# ymd_hms("2020-10-08T18:06:08.000Z")
+# table(chat2021$CreatedDate)
+Date_Time3 = ymd_hms(chattran$CreatedDate)
+# # head(Date_Time) #2020-07-02 21:56:42 UTC
+# chattran <- dplyr::mutate(chattran, Date_Time3)
+# 
+# # change to AZ time 
+# t3 <- as.POSIXct(Date_Time3, tz = "GMT")
+# attributes(t3)$tzone
+# AZ_time3 <- lubridate::with_tz(t3, "MST")
+# 
+# chattran <- dplyr::mutate(chattran, AZ_time3)
+
+# # convert time to decimals 
+# tm3.dechr <- hour(chattran$AZ_time) + minute(chattran$AZ_time)/60 + second(chattran$AZ_time)/3600
+# # tm1.dechr
+# 
+# chattran <- dplyr::mutate(chattran, tm3.dechr) 
+chattran <- chattran %>%
+     mutate(AZ_date3 = as.Date(AZ_time3))
+# chattran <- subset(chattran, select = -c(1))
+
+# write.csv(chattran, file = "chattran.csv")
+
+
+
+
